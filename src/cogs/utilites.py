6ebd092958828f8c97bc894e9475e65854904612
Juniper Watson <lyravox1@gmail.com>
2024-03-11 19:19:36 -0500

    Edit utilites.py

--- src/cogs/utilites.py
+++ src/cogs/utilites.py
@@ -6,5 +6,15 @@ class Utilites(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+    @nextcord.slash_command(description="Sends a list of the bot's commands")
+    async def help(self, interaction: Interaction):
+                
+        embed = nextcord.Embed(
+            color=0x7a0608
+        )
+        embed.add_field(name="", value="No commands... as of now ;)")
+        
+        await interaction.response.send_message(embed=embed)
+        
 def setup(bot):
     bot.add_cog(Utilites(bot))
