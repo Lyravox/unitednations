f5b7534ae843bcd0d7ebf8c84d54c6d3a833f018
Juniper Watson <lyravox1@gmail.com>
2024-03-11 23:53:02 -0500

    Edit utilites.py

--- src/cogs/utilites.py
+++ src/cogs/utilites.py
@@ -9,12 +9,15 @@ class Utilites(commands.Cog):
     @nextcord.slash_command(description="Sends a list of the bot's commands")
     async def help(self, interaction: Interaction):
                 
-        embed = nextcord.Embed(
-            color=0x7a0608
-        )
+        embed = nextcord.Embed(color=0x7a0608)
         embed.add_field(name="", value="No commands... as of now ;)")
         
         await interaction.response.send_message(embed=embed)
+        
+    @nextcord.slash_command(description="Givews bot latency")
+    async def ping(self, interaction: Interaction):
+        latency = round(self.bot.latency * 1000)
+        await interaction.response.send_message(f"Pong! My ping is currently {latency} ms.")
         
 def setup(bot):
     bot.add_cog(Utilites(bot))
